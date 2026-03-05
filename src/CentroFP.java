8b97594e62f935611f10900a79b0bd5c4184a9aa
Francisco Remiro <fremigom@myuax.com>
2026-03-05 08:52:58 +0100

    feat (CentroFP): Creada la clase CentroFP con su constructor creado y el método buscarAlumno(int id)

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class CentroFP {
+
+    private Alumno[] alumnos;
+    private final int MAX_ALUMNOS;
+
+    /**
+     * Constructor de la clase CentroFP
+     * @param MAX_ALUMNOS maximo de alumno de el array alumnos
+     */
+    public CentroFP(int MAX_ALUMNOS) {
+        this.MAX_ALUMNOS = MAX_ALUMNOS;
+        alumnos = new Alumno[MAX_ALUMNOS];
+    }
+
+    /**
+     * Busca al alumno por un id recibido
+     * @param id Identificador del alumno
+     * @return Devuelve un alumno si lo ha encontrado o sino un null si no lo ha encontrado
+     */
+    public Alumno buscarAlumno(int id){
+        boolean encontrado = false;
+        Alumno alumno = null;
+
+        for (int i = 0; i < MAX_ALUMNOS && !encontrado; i++){
+            if (alumnos[i].getId() == id){
+                alumno = alumnos[i];
+                encontrado = true;
+            }
+        }
+        return alumno;
+    }
+
+
+
+
+
 }
